efdc7a39d6400f0ae6ed39e6659e02d7f5da3031
Ashlee <ashleeryan@outlook.com>
2021-03-27 13:59:00 +0000

    added realtime listener, intialised orderBy on firestore to sort by created_on date

--- scripts/chat.js
+++ scripts/chat.js
@@ -5,6 +5,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats')
+        this.unsub;
     }
     async addChat(message){
         const now = new Date();
@@ -17,8 +18,35 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback){
+        this.unsub = this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added'){
+                    callback(change.doc.data())
+
+                }
+            })
+        })
+    }
+    updateName(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated')
+        this.unsub()
+    }
 }
 
 const chatroom = new Chatroom('crypto', 'ash')
+
+chatroom.getChats((data) => {
+    console.log(data)
+})
+
+chatroom.updateRoom('javascript')
 
 chatroom.addChat("hello this is a test").then(() => console.log("success")).catch(err => console.log("Houston we have a problem"))
